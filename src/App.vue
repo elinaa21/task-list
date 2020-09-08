b3456894cf82324d201808a0f68264d782533f1c
Elina <ella-abella@mail.ru>
2020-09-08 14:37:00 +0300

    beginning of layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <h1></h1>
-    <h1></h1>
-    <List />
+  <div class="app">
+    <div class="app__task-field">
+      <h1>Thursday, 8 September 2020</h1>
+      <h1></h1>
+      <List />
+    </div>
   </div>
 </template>
 
@@ -18,5 +20,24 @@ export default {
 </script>
 
 <style lang="scss">
+.app {
+  background-image: url('./img/ocean.jpg');
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
+  &__task-field {
+    background-color: rgba(255, 255, 255, 0.596);
+    width: 70%;
+    height: 80%;
+    text-align: center;
+    border-radius: 12px;
+  }
+}
 </style>
