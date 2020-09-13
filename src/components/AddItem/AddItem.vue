45b44cb0c55a187aa5b2751b9604c5e90f3a0d97
Elina <ella-abella@mail.ru>
2020-09-13 23:53:41 +0300

    add task by enter

--- src/components/AddItem/AddItem.vue
+++ src/components/AddItem/AddItem.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="add-item">
-    <input type="text" class="add-item__input" v-model="title" />
+    <input
+      type="text"
+      class="add-item__input"
+      v-model="title"
+      @keyup.enter="addItem"
+    />
     <button class="add-item__button" @click="addItem">&plus;</button>
   </div>
 </template>
