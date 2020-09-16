3c6b92b22adbbe88ea88aaae7c309a11d3d823e8
Elina <ella-abella@mail.ru>
2020-09-17 01:16:06 +0300

    unit testing

--- tests/unit/TodoItem.spec.js
+++ tests/unit/TodoItem.spec.js
@@ -1,16 +1,29 @@
 import { mount } from '@vue/test-utils';
-import AddItem from '@/components/AddItem/AddItem';
+import TodoItem from '@/components/TodoItem/TodoItem';
 
 describe('Task component unit tests', () => {
   it('is a Vue instance', () => {
-    const task = {
+    const todo = {
       id: 99988894,
       title: 'meeting',
       completed: false
     };
-    const wrapper = mount(AddItem, {
-      propsData: { task }
+    const wrapper = mount(TodoItem, {
+      propsData: { todo }
     });
     expect(wrapper.isVueInstance).toBeTruthy();
+  });
+
+  it('renders the title of task', () => {
+    const title = 'meeting';
+    const todo = {
+      id: 99988894,
+      title,
+      completed: false
+    };
+    const wrapper = mount(TodoItem, {
+      propsData: { todo }
+    });
+    expect(wrapper.html()).toContain(title);
   });
 });
